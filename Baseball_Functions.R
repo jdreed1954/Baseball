6f4052a73fb4e4370b79f964c8f1fd7894ef02da
Jim Reed <jdreed@q.com>
2017-05-08 15:39:07 -0600

    Checkpoint on work on the Bonds R Markdown.

--- Baseball_Functions.R
+++ Baseball_Functions.R
@@ -1,12 +1,19 @@
 
+Root <- "C:/Users/jdreed/Desktop/MyProjects/Baseball/baseballdatabank-2017.1/core/"
 # Here are some R functions which we will use to gather some specific data values about a player.
 getMaster <- function() {
-  M <- read_csv(file = "baseballdatabank-master/core/Master.csv",progress = FALSE )
+  MasterFile <- paste(Root, "Master.csv", sep ="")
+  M <- read_csv(file = MasterFile, progress = FALSE )
   return(M)
 }
 
+getPlayerID <- function(First, Last) {
+   player.id <- subset(Master, nameLast == Last & nameFirst == First)$playerID
+   return(player.id)
+}
 getBatting <- function() {
-  B <- read_csv(file = "baseballdatabank-master/core/Batting.csv",progress = FALSE )
+  BattingFile <-  paste(Root, "Batting.csv", sep = "")
+  B <- read_csv(file = BattingFile ,progress = FALSE )
   names(B)[names(B) == "3B"] <- "X3B"
   names(B)[names(B) == "2B"] <- "X2B"
   
@@ -18,15 +25,23 @@ getBatting <- function() {
 }
 
 getFielding <- function() {
-  F <- read_csv(file = "baseballdatabank-master/core/Fielding.csv",progress = FALSE )
+  FieldingFile <- paste(Root,"Fielding.csv", sep="")
+  F <- read_csv(file = FieldingFile, progress = FALSE )
   return(F)
 }
 
-getBatting2k <- function(B) {
-  #AB.totals <- ddply(B, .(playerID), summarize, Career.AB = sum(B$AB, na.rm = TRUE))
+getBatting2k <- function() {
+  B <- getBatting()
   AB.totals <-  B %>% group_by(playerID) %>% summarize(Career.AB = sum(AB, na.rm = TRUE))
   B <- as_tibble(merge(B, AB.totals))
   B2K <- subset(B, Career.AB >= 2000)
+  
+  player.list <- as.character(unique(B2K$playerID))
+  birthyears <-  sapply(player.list, get.birthyear)
+  B2K <-  merge(B2K, data.frame(playerID=player.list, Birthyear=birthyears))
+  B2K$Age <- with(B2K, yearID - Birthyear)
+  B2K <-  B2K[complete.cases(B2K$Age),]
+  
   return(B2K)
 }
 
@@ -39,7 +54,7 @@ find.position <- function(p) {
   positions[FLD == max(FLD)][1]
 }
 
-get_birthyear <- function(player.id) {
+get.birthyear <- function(player.id) {
   
   player.record <- subset(Master, playerID == player.id)
   
@@ -50,7 +65,7 @@ get_birthyear <- function(player.id) {
 
 get_stats <- function(player.id) {
   d <- subset(Batting, playerID == player.id)
-  byear <- get_birthyear(player.id)
+  byear <- get.birthyear(player.id)
   d$Age <- d$yearID - byear
   d$Avg <- with(d, H / AB)
   d$SLG <- with(d, (H - X2B - X3B - HR + 2 * X2B + 3 * X3B + 4 * HR) / AB)
@@ -62,7 +77,37 @@ get_stats <- function(player.id) {
   d
 }
 
-get_carreer <- function(d) {
+CalcCareer2k <- function() {
+  C.totals <- Batting.2000 %>% summarize(
+    C.G =  sum(G, na.rm = TRUE),
+    C.AB = sum(AB, na.rm = TRUE),
+    C.R  = sum(R, na.rm = TRUE),
+    C.H  = sum(H, na.rm = TRUE),
+    C.2B = sum(X2B, na.rm = TRUE),
+    C.3B = sum(X3B, na.rm = TRUE),
+    C.HR = sum(HR, na.rm = TRUE),
+    C.RBI = sum(RBI, na.rm = TRUE),
+    C.BB = sum(BB, na.rm = TRUE),
+    C.SO = sum(SO, na.rm = TRUE),
+    C.SB = sum(SB, na.rm = TRUE),
+    C.AVG = C.H / C.AB,
+    C.SLG = (C.H - C.2B - C.3B - C.HR + 2 * C.2B + 3 * C.3B + 4 * C.HR) / C.AB,
+    C.OBP  = (C.H + C.BB) / (C.H + C.AB + C.BB + sum(SF, na.rm = TRUE)),
+    C.OPS  = C.OBP + C.SLG
+  )
+  
+  C.totals <- merge(C.totals,Fielding.2000)
+  C.totals$Value.POS <- with(C.totals,
+                             ifelse(POS == "C", 240,
+                             ifelse(POS == "SS", 168,
+                             ifelse(POS == "2B", 132,
+                             ifelse(POS == "3B", 84,
+                             ifelse(POS == "OF", 48,
+                             ifelse(POS == "1B", 12, 0)))))))
+  return(C.totals)
+}
+
+get_career <- function(d) {
   attach(d,warn.conflicts = FALSE)
   car <- tibble(
     "C.Y" = nrow(d),
@@ -86,6 +131,35 @@ get_carreer <- function(d) {
   return(car)
 }
 
+get_career_long <- function(d) {
+  attach(d,warn.conflicts = FALSE)
+  
+  birthyear <- get.birthyear(d$playerID[1])
+  car <- tibble(
+    "Grp" = subset(players,player.id == d$playerID[1])$Group,
+    "Last" = subset(Master,playerID == d$playerID[1])$nameLast,
+    "First" = subset(Master,playerID == d$playerID[1])$nameFirst,
+    "Age"   = 2017 - birthyear,
+    "Y" = nrow(d),
+    "C.G" =  sum(G, na.rm = TRUE),
+    "C.AB" = sum(AB, na.rm = TRUE),
+    "C.R"  = sum(R, na.rm = TRUE),
+    "C.H"  = sum(H, na.rm = TRUE),
+    "C.2B" = sum(X2B, na.rm = TRUE),
+    "C.3B" = sum(X3B, na.rm = TRUE),
+    "C.HR" = sum(HR, na.rm = TRUE),
+    "C.RBI"= sum(RBI, na.rm = TRUE),
+    "C.BB" = sum(BB, na.rm = TRUE),
+    "C.SO" = sum(SO, na.rm = TRUE),
+    "C.SB" = sum(SB, na.rm = TRUE),
+    "C.AVG" = C.H / C.AB,
+    "C.SLG" = (C.H - C.2B - C.3B - C.HR + 2 * C.2B + 3 * C.3B + 4 * C.HR) / C.AB,
+    "C.OBP"  = (C.H + C.BB) / (C.H + C.AB + C.BB + sum(SF, na.rm = TRUE)),
+    "C.OPS"  = C.OBP + C.SLG
+  )
+  detach(d)
+  return(car)
+}
 fit.model <- function(d) {
   fit <- lm(OPS ~I(Age - 30) + I((Age - 30)^2), data = d)
   b <- coef(fit)
@@ -134,7 +208,7 @@ collapse.stint <- function(d) {
              Career.AB=d$Career.AB[1], POS=d$POS[1])
 }
 
-fit.trajectory <-  function(d) {
+fit.traj <-  function(d) {
   fit <- lm(OPS ~ I(Age - 30) + I((Age - 30)^2), data = d)
   data.frame(Age = d$Age, Fit = predict(fit, Age = d$Age))
 }
@@ -150,9 +224,27 @@ plot.trajectories <-  function(first, last, n.similar=5, ncol){
   player.id <-   subset(Master, nameFirst == first & nameLast == last)$playerID
   player.id <-   as.character(player.id)
   player.list <- as.character(similar(player.id, n.similar)$playerID)
-  Batting.new <- subset(Batting.2000, playerID %in% player.list)
+  Batting.2000 <- getBatting2k()
+  #>>>>>>> Temporary
+  PLAYER <-  as.character(unique(Batting.2000$playerID))
+  POSITIONS <-  sapply(PLAYER,find.position)  # long running
+  Fielding.2000 <- tibble(playerID=names(POSITIONS), POS = POSITIONS)
+  Batting.2000 <-  as_tibble(merge(Batting.2000, Fielding.2000))
   
-  F2 <- Batting.new %>% group_by(playerID) %>% do(fit.trajectory(.))
+  # >>>>>>>>> End Temporary
+  
+  
+  # Add Age to Batting.2000 data table.
+  player.list <-  as.character(unique(Batting.2000$playerID))
+  birthyears <- sapply(player.list,get.birthyear)
+  Batting.2000 <-  merge(Batting.2000, data.frame(playerID=player.list, Birthyear=birthyears))
+  Batting.2000$Age <-  with(Batting.2000, yearID - Birthyear)
+  
+  Batting.new <- subset(Batting.2000, playerID %in% player.list) %>% 
+    group_by(playerID) %>% 
+    do(collapse.stint(.))
+  
+  F2 <- Batting.new %>% group_by(playerID) %>% do(fit.traj(.))
   F2 <-  merge(F2, 
             data.frame(playerID=player.list, 
             Name = sapply(as.character(player.list), get.name)))
@@ -169,4 +261,110 @@ summarize.trajectory <- function(d) {
   Age.max <- round(30- b[2] / b[3] /2, 1)
   Max <-  round(b[1] - b[2] ^ 2 / b[3] / 4, 3)
   data.frame(Age.max=Age.max, Max = Max, Curve=round(b[3], 5))
+}
+
+getRockiesGBG <- function(Yr) {
+  # Function retrieves the Game-by-Game from baseball-reference.com and re-formats
+  # so it is suitable for display.  Note, Classes variable below helps cast the various
+  # fields into a data type suitable to its purposes.
+  #
+  Classes <- c(
+    "integer",
+    rep("character", 6),
+    rep("integer", 3),
+    "character",
+    rep("integer", 2),
+    rep("character", 5),
+    "integer",
+    "character"
+  )
+  
+  URL <- paste("http://www.baseball-reference.com/teams/COL/", Yr, 
+               "-schedule-scores.shtml#team_schedule::none", sep="")
+  
+  gbgurl <- getURL(URL)
+  
+  tables <-
+    readHTMLTable(gbgurl, stringsAsFactors = FALSE, colClasses = Classes)
+  
+  tables <- list.clean(tables, fun = is.null, recursive = FALSE)
+  
+  n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+  colnames(tables$team_schedule)[3] <- "X3"
+  colnames(tables$team_schedule)[5] <- "H/A"
+  
+  G <-  tables$team_schedule %>% filter(Date != "Date")
+  
+  G <-  as_tibble(G) %>% select(-c(3, 4, 10:13, 17, 19)) %>% 
+    separate(Date, into = c("Day", "Date"), sep = ",")
+
+  G$Day <- strtrim(G$Day, 3)
+  
+ # G <- mutate(G, `H/A` = ifelse(G$`H/A` == "@", "A", "H")) %>% 
+ #  filter(!is.na(Win))
+  BLANK <- " "
+  G <- mutate(G, `H/A` = ifelse(G$`H/A` == "@", "A", "H"))
+  n.col <- ncol(G)
+  for ( i in 1:nrow(G)) {
+    if (is.na(G$Win[i])) {
+      G[i,6] <- BLANK
+      G[i,7:8] <- 0
+      G[i,9:n.col] <- BLANK
+    }
+  }
+  
+  return(G)
+}
+
+getSalaries <- function(Yr) {
+  # Note, this website is only good back to the 2000 season.
+  FieldClasses <- c("integer","character", "numeric", rep("character",6))
+  
+  
+  url <- paste("http://www.spotrac.com/mlb/payroll/", Yr, "/", sep = "")
+  Sal <- getURL(url)
+  
+  tables <- readHTMLTable(Sal, colClasses = FieldClasses,
+                          stringsAsFactors = FALSE)
+  tables <- list.clean(tables, fun = is.null, recursive = FALSE)
+  
+  s <- as_tibble(tables$`NULL`)
+  s <- filter(s,Rank != is.na(Rank))
+  s <- rename(s, X25.Man = `25-Man`, Disabled.List = `Disabled List`, 
+              Total.Payroll = `Total Payroll`)
+  #
+  s$X25.Man <- with(s, X25.Man <-  sub("\\$", "", X25.Man)) %>% 
+    gsub(",","",.) %>% sub("0-", "0", .)
+  s$Disabled.List <- with(s, Disabled.List <-  sub("\\$", "", Disabled.List)) %>%    
+    gsub(",","",.) %>% sub("0-", "0", .)
+  s$Retained <- with(s, Retained <-  sub("\\$", "", Retained)) %>%
+    gsub(",","",.) %>% sub("0-", "0", .)
+  s$Buried <- with(s, Buried <-  sub("\\$", "", Buried)) %>%
+    gsub(",","",.) %>% sub("0-", "0", .)
+  s$Suspended <- with(s, Suspended <-  sub("\\$", "", Suspended)) %>%
+    gsub(",","",.) %>% sub("0-", "0", .)
+  s$Total.Payroll <- with(s, Total.Payroll <-  sub("\\$", "", Total.Payroll)) %>% 
+    gsub(",","",.) %>% sub("0-", "0", .)
+  # Now we convert character columns corresponding to numbers.
+  s <- as_tibble(type_convert(s)) 
+  return(s)
+  
+}
+
+getCurrentTeams <- function() {
+  c.Teams <- read_csv("../Baseball/Teams_Current.csv", trim_ws = TRUE)
+  return(c.Teams)
+}
+
+getCurrentResults <- function(){
+  GBG17 <- getRockiesGBG(2017) 
+  GBG17 <- filter(GBG17, Win != " ")  
+  C.Results <- tibble(
+    Wins =  length(grep("W",GBG17$`W/L`)),
+    Losses =  length(grep("L",GBG17$`W/L`)),
+    Win.Pct = Wins/(Wins + Losses) * 100,
+    Runs = sum(as.integer(GBG17$R)),
+    Runs.Ag = sum(as.integer(GBG17$RA)),
+    `Predicted Season Wins` = round(Runs^2.00/(Runs^2.00+Runs.Ag^2.00)*162))
+  return(C.Results)
 }
